99278a8c556431bbc0c3e0eb7dcff3ccb969ecbe
Tomos Wootton <tomoswootton@gmail.com>
2016-12-08 23:58:20 +0000

    runThroughQueue method added

--- Grid.java
+++ Grid.java
@@ -1,63 +1,188 @@
-import java.util.Arraylist;
+import java.util.ArrayList;
 
 class Grid {
 
-  private int counter;
+  private static String initialState;
+  private static String goalState;
 
-  public Grid() {
-    this.inputBoard();
-    return;
+  public String move;
+  private String state;
+  private Grid parent;
 
-    Arraylist<String> queue = new Arraylist<String>();
-    Arraylist<String> moves = new Arraylist<String>();
-    // this.getStartBoard();  //sets initialState variable
-    // this.getEndBoard(); //sets goalState variable
-    this.counter = 0;  //start counter at 0
-    //
-    // //code will find every possible move,
-    // while(true){    //continue looping until break is reached
-    //
-    //   this.currentNode = queue.get(this.counter); // set currentNode to queue
-    //   //array item with intex counter
-    //   //find, check for presence and add for Lmove;
-    //   String lmove = getLMove(this.currentNode);
-    //   if(lmove == this.goalState){
-    //     moves.add(lmove);
-    //     System.out.println("Goal state reached");
-    //     break;
-    //   } else if(!queue.contains(lmove) && lmove != null){
-    //     queue.add(lmove);
-    //     moves.add("L");
-    //   }
-    //   //find, check for presence and add for Rmove
-    //   String rmove = findRmove();
-    //   if(rmove == this.goalState){
-    //     moves.add(rmove);
-    //     break;
-    //   } else if(!queue.contains(rmove) && rmove != null){
-    //     queue.add(rmove);
-    //     moves.add("R");
-    //   }
-    //   //find, check for presence and add for Umove
-    //   String umove = findUmove();
-    //   if(umove == this.goalState){
-    //     moves.add(this.Umove);
-    //     break;
-    //   } else if(!queue.contains(umove) && umove != null){
-    //     queue.add(umove);
-    //     moves.add("U");
-    //   }
-    //   //find, check for presence and add for Dmove
-    //   String dmove = getLMove();
-    //   if(dmove == this.goalState){
-    //     moves.add(dmove);
-    //     break;
-    //   } else if(!queue.contains(dmove) && dmove != null){
-    //     queue.add(dmove);
-    //     moves.add("D");
-    //   }
-    //   counter();
-    // }
+  public Grid(){
+    this.state = Grid.getInitialState();
   }
 
+  //set variables of new grid instance.
+  //parent parameter gives state of parent grid
+  //move parameter is which child the new grid is eg L, R, U or D
+  public Grid(Grid parent, String move) {
+    this.move = move;
+    this.parent = parent;
+    //getState applys each possible move to current grid, creating new grids.
+    this.state = parent.getState();
+  }
+
+  public static String getInitialState(){
+    return Grid.initialState;
+  }
+
+  public static void setInitialState(String initialState){
+    Grid.initialState = initialState;
+  }
+
+  public static String getGoalState(){
+    return Grid.goalState;
+  }
+
+  public static void setGoalState(String goalState){
+    Grid.goalState = goalState;
+  }
+
+  //return state of parent if no move parameter is given
+  public String getState(){
+    if (this.move != null) {
+      return this.getMovedState();
+    } else {
+      return this.state;
+    }
+  }
+
+  public String getMovedState() {
+    return this.applyMove();
+  }
+
+  //check possible move is available. eg if move is L, no possible move if the
+  //empty("0") index is 0, 3 or 6 becasue they are the left side of the grid
+  public boolean valid() {
+    int emptyIndex = this.findEmptySpace();
+    if (this.move == "U" && emptyIndex <= 2) return false;
+    if (this.move == "D" && emptyIndex >= 6) return false;
+    if (this.move == "L" && emptyIndex % 3 == 0) return false;
+    if (this.move == "R" && emptyIndex % 3 == 2) return false;
+    return true;
+  }
+
+  //find each child of current grid
+  public String applyMove(){
+    switch(this.move){
+      case "L":
+        return findLMove();
+      case "R":
+        return findRMove();
+      case "U":
+        return findUMove();
+      case "D":
+        return findDMove();
+    }
+    return "";
+  }
+
+  //method returns board with 0 piece moved 1 place left;
+  public String findLMove(){
+    int emptyIndex = this.findEmptySpace();
+    if (this.valid() == false) {
+      return null;
+    } else {
+      //if move is possible, swap 0 with item in index of 0 - 1
+      return this.swapItems(emptyIndex-1);
+    }
+  }
+
+  //method returns board with 0 piece moved 1 place right;
+  public String findRMove(){
+    int emptyIndex = this.findEmptySpace();
+    //dont apply left move if index 2, 5 or 8 because no possible move is available
+    if (this.valid() == false) {
+      return null;
+    } else {
+      //if move is possible, swap 0 with item in index of 0 + 1
+      return this.swapItems(emptyIndex+1);
+    }
+  }
+
+  //method returns board with 0 piece moved 1 place up;
+  public String findUMove(){
+    int emptyIndex = this.findEmptySpace();
+    //dont apply left move if index 0, 1 or 2 because no possible move is available
+    if (this.valid() == false) {
+      return null;
+    } else {
+      //if move is possible, swap 0 with item in index of 0 - 3
+      return this.swapItems(emptyIndex-3);
+    }
+  }
+
+  //method returns board with 0 piece moved 1 place down;
+  public String findDMove(){
+    int emptyIndex = this.findEmptySpace();
+    //dont apply left move if index 6, 7 or 8 because no possible move is available
+    if (this.valid() == false) {
+      return null;
+    } else {
+      //if move is possible, swap 0 with item in index of 0 + 3
+      return this.swapItems(emptyIndex+3);
+    }
+  }
+
+  //finds index of 0 so that it can be swapped with item in space L R U or D of it
+  public int findEmptySpace(){
+    return this.state.indexOf("0");
+  }
+
+  //swap 0 item with item in L R D or U space of it
+  public String swapItems(int index){
+    //turn string in to array for swap of characters
+    char[] str = this.state.toCharArray();
+    char char1 = this.state.charAt(findEmptySpace());
+    char char2 = this.state.charAt(index);
+    //in the index of 0, put char2 (item to be swapped with)
+    str[findEmptySpace()] = char2;
+    //in the index of the item to be swapped with, put char1(0)
+    str[index] = char1;
+    return String.valueOf(str);
+  }
+
+  //prints string out, used for testing and for outputs.
+  public String toString(){
+    StringBuilder str = new StringBuilder();
+    char[] arr =  this.getState().toCharArray();
+    for( int i = 0; i<arr.length; i++ ){
+      if (arr[i] == '0') {
+        str.append(' ');
+      } else {
+        str.append(arr[i]);
+      }
+      if(i % 3 == 2){
+        str.append('\n');
+      }
+    }
+    return String.valueOf(str);
+  }
+
+//method finds children of Grid object and adds them to queue or ends program if
+//goal state is reached.
+public ArrayList<Grid> getChildren(){
+  //for each grid, create array of each possible move that can be taken
+  ArrayList<Grid> grids = new ArrayList<Grid>();
+  for (String move: new String[]{"L", "R", "U", "D"}) {
+    Grid newGrid = new Grid(this, move);
+      //if new grids state is the same as the goal state, end program because solution has been found
+    if (newGrid.state == this.getGoalState()){
+      this.endProgram(newGrid.state);
+      //checks move is possible and if newGrid is already in the queue because if so
+      //it shouldnt be added.
+    }else if (newGrid.valid() && Main.queue.contains(newGrid) == false) {
+      grids.add(newGrid);
+      Main.moves.add(move);
+    }
+    }
+  return grids;
+}
+//method stops program when goal state is reached
+public void endProgram(String state){
+  System.out.println("state of winner: "+ state);
+  System.exit(0);
+
+}
 }
--- Main.java
+++ Main.java
@@ -1,18 +1,67 @@
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Arraylist;
 
 public class Main {
 
 // *********** INITIALISING**************
-  public String initialState;
-  public String goalState;
-  public String currentNode;
-  public int counter;
 
-  static Scanner userInput = new Scanner(System.in);
+// create scanner for inputs
+static Scanner userInput = new Scanner(System.in);
+
+public static ArrayList queue = new ArrayList();
+public static ArrayList moves = new ArrayList();
 
   public static void main(String[] args){
-    Grid grid = new Grid();
+    //inputs initial board and goal board states
+    //String initialState = Main.inputBoard("enter initial");
+    //String goalState = Main.inputBoard("enter goal");
+    //Grid.setInitialState(initialState);
+    Grid.setInitialState("123405678");
+    //Grid.setGoalState(goalState);
+    Grid.setGoalState("123450678");
+
+
+    //for initial board state
+    //add each initail move to queue
+    for (String move: new String[]{"L", "R", "U", "D"}){
+      //create grid of each move
+      Grid grid = new Grid(new Grid(), move);
+      //add each grid to queue
+      queue.add(grid);
+    }
+    //with the first 4 moves added, run through queue finding children of each
+    //item and check if each item is equal to the goal state
+    Main.runThroughQueue();
+
   }
 
+  public static void runThroughQueue(){
+    //loop until i is the length of the queue, which will only happen if no solution is found
+    int i = 0;
+    while (i < queue.size()) {
+      //grab first item in queue array
+      Grid currentGrid = queue.get(0);
+      //apply getChidlren method to item, this method will stop the program if
+      //one of the children is equal to goal state
+      currentGrid.getChildren();
+      i++;
+    }
+    System.out.println("queue" + queue);
+    System.out.println("no solution found");
+    System.exit(0);
+  }
+
+  //inputs code ensures inputs are not null and of length 9
+  private static String inputBoard(String msg){
+    String inp = null;
+    do {
+      System.out.println(msg);
+      if (userInput.hasNextLine()) {
+        inp = userInput.nextLine();
+      }
+    } while(inp.length() != 9);
+    return inp;
+  }
 }
+//line 47, want ot put getChildren method on value of i in queue
+//whats tosTring for
